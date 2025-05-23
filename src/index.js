c3cc52297465ce79a02d8271a5d233aa65c92b3b
Agustin Allende <agus.allende996@gmail.com>
2025-05-23 18:35:05 -0300

    Uso de variable de entorno para login con Google

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ passport.use(new GoogleStrategy(
   {
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:8000/auth/google/perfil"
+    callbackURL: `${process.env.BASE_URL}/auth/google/perfil`
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
